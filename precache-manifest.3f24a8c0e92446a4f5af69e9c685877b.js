3c9b7889af65bff8e8316ef8067c922a29251686
Anh Ho <hoquocanhxxx@gmail.com>
2022-06-16 16:44:44 +0700

    Updates

---
+++ precache-manifest.3f24a8c0e92446a4f5af69e9c685877b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbc6185534e0e861e05e",
+    "url": "/static/css/main.b9d73b96.chunk.css"
+  },
+  {
+    "revision": "cbc6185534e0e861e05e",
+    "url": "/static/js/main.cbc61855.chunk.js"
+  },
+  {
+    "revision": "926454176694cbfe7ed4",
+    "url": "/static/js/1.92645417.chunk.js"
+  },
+  {
+    "revision": "9c528c2cd85b1ea8da16",
+    "url": "/static/css/2.1716e0cc.chunk.css"
+  },
+  {
+    "revision": "9c528c2cd85b1ea8da16",
+    "url": "/static/js/2.9c528c2c.chunk.js"
+  },
+  {
+    "revision": "ae9ab70b8d8207809937",
+    "url": "/static/js/runtime~main.ae9ab70b.js"
+  },
+  {
+    "revision": "61fb15c0faf238151f995f7f26016a34",
+    "url": "/index.html"
+  }
+];
