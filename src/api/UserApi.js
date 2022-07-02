c17dad630670520e608e31e91c5771a70d27753f
hoquocanh <hoquocanhxxx@gmail.com>
2022-07-02 10:18:14 +0700

    test deloy lên github

---
+++ src/api/UserApi.js
@@ -0,0 +1,24 @@
+import axiosClient from "./axiosClient";
+const UserApi = {
+  getAll(params) {
+    const url = "/user";
+    return axiosClient.get(url, { params });
+  },
+  get(id) {
+    const url = `/user/${id}`;
+    return axiosClient.get(url);
+  },
+  add(data) {
+    const url = "/user";
+    return axiosClient.post(url, data);
+  },
+  update(data) {
+    const url = `/user/${data.id}`;
+    return axiosClient.patch(url, data);
+  },
+  remove(id) {
+    const url = `/user/${id}`;
+    return axiosClient.delete(url);
+  },
+};
+export default UserApi;
